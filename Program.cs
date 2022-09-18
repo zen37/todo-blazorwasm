055e8e102410a69fd71f285fb76e3e4a8925d209
zen37 <zen37>
2022-09-18 07:47:47 -0700

    docs

--- Program.cs
+++ Program.cs
@@ -2,10 +2,31 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using todo_blazorwasm;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+    Console.WriteLine("blazor app started ...");
 
-await builder.Build().RunAsync();
+// created by 'dotnet new blazorwasm'
+    var builder = WebAssemblyHostBuilder.CreateDefault(args);
+    builder.RootComponents.Add<App>("#app");
+    builder.RootComponents.Add<HeadOutlet>("head::after");
+
+   builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+    await builder.Build().RunAsync();
+
+/* works too
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        Console.WriteLine("blazor app started ...");
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+        //builder.RootComponents.Add<HeadOutlet>("head::after");
+
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+        await builder.Build().RunAsync();
+    }
+}
+*/
